8895927c1ee3c1755d927647ff172ef9da4351d4
WHM0304 <whm0304@naver.com>
2024-01-07 23:35:43 +0900

    프로젝트

---
+++ UIUX/js/nav.js
@@ -0,0 +1,12 @@
+const main_nav = document.querySelector("div.control");
+const body = document.querySelector(".body");
+main_nav.addEventListener("click", (event) => {
+  const target = event.target;
+  const tag_name = event.target.tagName;
+  const class_name = target.className;
+  if (tag_name === "I") {
+    if (class_name == "fa-solid fa-calendar-days") {
+      importHTML(body, "./calendar.html");
+    }
+  }
+});
